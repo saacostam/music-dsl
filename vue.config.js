aa5341f49cce4b3c35aa8b8fc89eb1ada3940342
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2022-11-26 00:18:26 -0500

    keep classnames 🎈

--- vue.config.js
+++ vue.config.js
@@ -9,8 +9,19 @@ module.exports = {
             // Some language extensions like typescript are so huge that may impact build performance
             // e.g. Build full languages support with webpack 4.0 takes over 80 seconds
             // Languages are loaded on demand at runtime
-            languages: ['javascript', 'css', 'html', 'typescript']
+            // languages: ['javascript']
           })
     ]
   },
+
+  chainWebpack: config => {
+    config.optimization
+      .minimizer('terser')
+      .tap(args => {
+        const { terserOptions } = args[0]
+        terserOptions.keep_classnames = true
+        terserOptions.keep_fnames = true
+        return args
+      })
+   }
 }
