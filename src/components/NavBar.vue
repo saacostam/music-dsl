790e274c1db41fd0e6a0b9a719c1d55c54240e5d
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2022-11-27 14:52:49 -0500

    documentation links added

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,6 +3,11 @@
         <span class="navbar-brand mb-0" style="font-size: 1rem; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">
             <span style="color: #007bff">TUNE</span>CODE
         </span>
+
+        <div style="margin-left: auto; font-size: 0.8rem; color: white;">
+            <router-link to="/" style="margin-right: 1rem;">Editor</router-link>
+            <router-link to="documentation">Documentación</router-link>
+        </div>
     </nav>
 </template>
 
@@ -17,5 +22,9 @@ export default{
         height: 3rem;
         background-color: #1e1e1e;
         border-bottom: 1px #737373 solid;
+    }
+    a{
+        text-decoration: none;
+        color: white;
     }
 </style>
